437e83f2a3cc3379e513449f2df1615f0f4e100d
Roberto Salinas <robertosalinasaguilar99@gmail.com>
2024-04-07 11:44:01 -0300

    feat: moved app to port 8000

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.get('/ping', (req, res) => {
 	res.send('pong');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
